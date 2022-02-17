2623a57391d89c2e0f4dfef37fd454b9d4ebc436
twistinside <twistinside@users.noreply.github.com>
2022-02-17 11:03:09 -0800

    Initial Commit

---
+++ Kestrel/KestrelApp.swift
@@ -0,0 +1,17 @@
+//
+//  KestrelApp.swift
+//  Kestrel
+//
+//  Created by Karl Groff on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct KestrelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
