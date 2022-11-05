6261fab97c77efa70fedda03f5d1df3aee394a8a
twistinside <twistinside@users.noreply.github.com>
2022-11-04 22:20:19 -0700

    Add input controller

--- Kestrel/Services/Service Locator.swift
+++ Kestrel/Services/Service Locator.swift
@@ -1,11 +1,14 @@
 // This class is based on reading done at https://gameprogrammingpatterns.com/service-locator.html
+import Touchy
 
 class ServiceLocator {
     static let shared = ServiceLocator()
 
+    private(set) var inputController: TCHInputController
     private(set) var renderer: Renderer
 
     private init() {
+        self.inputController = TCHStandardInputController()
         self.renderer = Renderer()
     }
 }
